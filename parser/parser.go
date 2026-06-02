2406f2bfcb2c9caf99b200bb02e662b92ff2174b
Arctan <175182882+aarctanz@users.noreply.github.com>
2026-06-02 16:11:06 +0530

    verify valid url

--- parser/parser.go
+++ parser/parser.go
@@ -1,12 +1,30 @@
 package parser
 
 import (
+	"errors"
 	"net/http"
 	"net/url"
+	"regexp"
 	"time"
 
 	"golang.org/x/net/html"
 )
+
+var (
+	URLRegex = regexp.MustCompile(`[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
+)
+
+func verifyValidURL(rawUrl string, base *url.URL) (*url.URL, error) {
+	parsedUrl, err := base.Parse(rawUrl)
+	if err != nil {
+		return nil, err
+	}
+	if URLRegex.MatchString(parsedUrl.String()) {
+		return parsedUrl, nil
+	}
+
+	return nil, errors.New("Invalid Url")
+}
 
 func Parse(resp *http.Response, base *url.URL) ([]string, time.Duration) {
 	now := time.Now()
@@ -29,7 +47,7 @@ func Parse(resp *http.Response, base *url.URL) ([]string, time.Duration) {
 				for {
 					k, v, more := z.TagAttr()
 					if string(k) == "href" {
-						link, err := base.Parse(string(v))
+						link, err := verifyValidURL(string(v), base)
 						if err != nil {
 							break
 						}
